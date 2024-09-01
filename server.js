37b0938779b019181c1af241fc7665d5f354676c
Elham <elhamwaheed777@gmail.com>
2024-09-02 04:10:47 +0500

    work afte navbar

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 let menuIcon = document.querySelector(".menu-icon");
 let menuElements = document.querySelector(".menu_elements");
+
 menuIcon.addEventListener("click", () => {
-  menuIcon.classList.toggle("move");
-  menuElements.classList.toggle("active");
+  menuIcon.classList.toggle("move"); // Optional: To animate the icon
+  menuElements.classList.toggle("active"); // Toggle menu visibility
 });
